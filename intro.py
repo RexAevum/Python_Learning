f5ad0d3dac5479ea24c006adebdd6d04be60d0a2
Rolans A <rapin001@fiu.edu>
2020-12-01 19:02:09 -0500

    list mod

--- intro.py
+++ intro.py
@@ -5,7 +5,7 @@ print( "Current time: ", datetime.datetime.now())
 nowTime = datetime.datetime.now()
 print(nowTime)
 num = 2
-print(nowTime, " ", num, "\n")
+#print(nowTime, " ", num, "\n")
 
 # Types
 x = 10 # int
@@ -16,7 +16,7 @@ print(type(x), type(y), type(z))
 
 # Lists
 list1 = [10.0, 0, "OK"] # can have different types in the same list!!!
-print(list1)
+#print(list1)
 
 # Using built in functions
 student_grades = [75, 30, 99, 79]
@@ -27,18 +27,37 @@ gradeCount = student_grades.count(1)
 # To get item count in container need to use built in len() command
 # Can also be written using the builtins of pythone
 gradeCount2 = len(student_grades)
-print('\nGrades:', gradeCount, '\n', gradeCount2, '\n')
+#print('\nGrades:', gradeCount, '\n', gradeCount2, '\n')
 # get mean
 
 mean = gradeSum / gradeCount2
-print(mean, '\n')
+#print(mean, '\n')
 
 # List vs dictionary 
 exampleList = [1, 2, 3] # this is a list -> square brackets and each value is sepperated with comma
 
 exampleDictionary = {"Nr 1: " : 9.1, "Nr 2: " : 6.9, "Nr 3: " : 2.1}
-print(exampleDictionary, '\n')
+#print(exampleDictionary, '\n')
 
 # Tuple in dictionary 
 day_temperatures = {1 : (), '2' : (), '3' : ()}
-print(day_temperatures, '\n')
+#print(day_temperatures, '\n')
+
+# Modifying a list
+listToMod = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# add itme
+listToMod.append(7)
+# remove item
+listToMod.remove(7)
+
+# To access a specific range of indexes in the container 
+print(listToMod[1:3]) # The range can be longer than the lenght of the list, will just cover till last item
+
+# To access from a specific index to end of list
+listToMod[3:]
+
+# Negative indexes on a list of strings
+wordList = ['hello', 'bye', 'stay']
+
+print(wordList[0][2]) # the first square will get the 0 index item and the second square will get the letter at index 2 
